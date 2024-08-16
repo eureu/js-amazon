93de5a669c1f73ad137144efef4e51942d3035ae
eureu <venekbespalov@mail.ru>
2024-08-16 15:23:22 +0300

    add: some backend

--- data/products.js
+++ data/products.js
@@ -57,45 +57,32 @@ class Clothing extends Product {
 
 }
 
-const product1 = new Product(
-  {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: [
-      "socks",
-      "sports",
-      "apparel"
-    ]
-  },
-);
+class Appliance extends Product {
+  instructioinsLink;
+  warrantyLink;
 
-console.log(product1);
+  constructor(productDetails) {
+    super(productDetails);
+    this.instructioinsLink = productDetails.instructioinsLink;
+    this.warrantyLink = productDetails.warrantyLink;
+  }
 
-const product2 = new Product(
-  {
-    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 56
-    },
-    priceCents: 799,
-    keywords: [
-      "tshirts",
-      "apparel",
-      "mens"
-    ],
-    type: "clothing",
-    sizeChartLink: "images/clothing-size-chart.png"
-  },
-);
+  extraInfoHTML() {
+    return `
+      <a href="${this.instructioinsLink}" target="_blank">
+        Instructions
+      </a>
+
+      <a href="${this.warrantyLink}" target="_blank">
+        Warranty
+      </a>
+    `;
+  }
+
+}
+
+const date = new Date();
+console.log(date);
 
 
 export const products = [
@@ -158,7 +145,10 @@ export const products = [
       "toaster",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: 'appliance',
+    instructioinsLink: 'supersimple.dev/images/appliance-instructions.png',
+    warrantyLink: 'supersimple.dev/images/appliance-warranty.png'
   },
   {
     id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
@@ -760,10 +750,10 @@ export const products = [
 ].map((productDetails) => {
 
   if (productDetails.type === 'clothing'){
-    return new Clothing(productDetails)
+    return new Clothing(productDetails);
+  } else if (productDetails.type === 'appliance') {
+    return new Appliance(productDetails);
   }
 
   return new Product(productDetails);
 });
-
-console.log(products)
